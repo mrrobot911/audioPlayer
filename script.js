5c1579d3a10f2b5f83fb03112fecea5261d04e32
mrrobot911 <kelwin2705@gmail.com>
2023-09-22 21:26:42 +0300

    fix: mute bug

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function loadTrack(track_index){
 
     curr_track.src = music_list[track_index].music;
     curr_track.load();
-    setVolume();
+    isMute ? curr_track.volume = 0 : setVolume();
 
     const rotate = details(track_index);
     updateTimer = setInterval(setUpdate, 1000);
