c79de7893dacad934c21fbefb7e001ba323d3892
mrrobot911 <kelwin2705@gmail.com>
2023-09-24 12:10:00 +0300

    fix: time

--- script.js
+++ script.js
@@ -113,22 +113,20 @@ function muteVolume() {
     isMute ? setVolume() : curr_track.volume = 0;
     isMute = !isMute;
 }
+function minutes(time) {
+    const cur_minutes = Math.floor(time / 60);
+    return cur_minutes < 10 ? "0" + cur_minutes : cur_minutes;
+}
+function seconds(time) {
+    const cur_seconds = Math.floor(time - minutes(time) * 60);
+    return cur_seconds < 10 ? "0" + cur_seconds : cur_seconds;
+}
 function setUpdate(){
     if(!isNaN(curr_track.duration)){
         seek_slider.value = curr_track.currentTime * (100 / curr_track.duration) || 0;
 
-        let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
-
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-
-        curr_time.textContent = `${currentMinutes}:${currentSeconds}`;
-        total_duration.textContent = `${durationMinutes}:${durationSeconds}`;
+        curr_time.textContent = `${minutes(curr_track.currentTime)}:${seconds(curr_track.currentTime)}`;
+        total_duration.textContent = `${minutes(curr_track.duration)}:${seconds(curr_track.duration)}`;
     }
 }
 
